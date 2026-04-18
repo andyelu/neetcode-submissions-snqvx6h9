4a0f34578922e6869ceaa622399c732c60b44424
andyelu <126619706+andyelu@users.noreply.github.com>
2026-04-18 16:10:04 -0700

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substring_chars = {}
+
+        res = 0
+
+        l,r = 0,0
+
+        # increment r each iteration -- check if s[r] in the set. If it is
+        # we increment the l pointer until s[l] == s[r] -- while we do this
+        # remove s[l] from the set
+
+        # instead of using a set, we could use a hashmap to map char to index,
+        # which will make the iteration logic a lil cleaner
+
+        while r < len(s):
+            if s[r] in substring_chars:
+                new_pos = substring_chars[s[r]] + 1
+                while l < new_pos: 
+                    # along the way, clear the mappings that become obsolete
+                    substring_chars.pop(s[l])
+                    l += 1
+
+            substring_chars[s[r]] = r
+            r += 1
+
+            res = max(res, r-l)
+        return res
